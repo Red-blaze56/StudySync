8f23fe11cc8ce38a4d04d19182ef984a637a640d
Red-blaze56 <ashutoshasthana56@gmail.com>
2025-12-14 03:31:15 +0530

    add has_sufficient_text() logic to see if the text of doc needs ocr or not

--- src/Processors/pdf_processor.py
+++ src/Processors/pdf_processor.py
@@ -8,17 +8,21 @@ from langchain_community.document_loaders import PyMuPDFLoader
 
 
 def load_pdf(pdf_path: Path) -> List[Document]:
-    if is_pdf_scanned(pdf_path):
-        return _extract_text_direct(pdf_path)
-    else:
-        return _extract_text_from_ocr(pdf_path)
+    docs = _extract_text_direct
+    
+    if not _has_sufficient_text(pdf_path):
+        return _extract_text_from_ocr
+    
+    return docs
     
 def _extract_text_direct(pdf_path: Path) -> List[Document]:
-    loader = PyMuPDFLoader()
+    loader = PyMuPDFLoader(str(pdf_path))
     docs = loader.load(pdf_path)
 
     for doc in docs:
-        doc.metadata.update =({"source_type" : "PDF", "file_name" : pdf_path.name})
+        doc.metadata.update =({
+            "source_type" : "PDF", "file_name" : pdf_path.name
+        })
     
     return docs
 
@@ -28,14 +32,24 @@ def _extract_text_from_ocr(pdf_path: Path) -> List[Document]:
     if not text.strip():
         return []
     
-    return Document(
-        page_content=text,
-        metadata={"source_type" : "PDF", "file_name" : pdf_path.name}
-    )
+    return [
+        Document(
+            page_content=text,
+            metadata={
+                "source_type" : "PDF",
+                "file_name" : pdf_path.name
+            }
+        )
+    ]
 
-'-------------------------------------------------------------------------------------------------------'
+def _has_sufficient_text(docs: List[Document]) ->bool:
+    for d in docs:
+        total = sum(len(d.page_content.strip()))
+        avg_char_per_page=total/max(len(docs),1)
 
-def is_pdf_scanned(pdf_path: Path) -> bool:
-    pass
+    if total>=200 and avg_char_per_page>=40:
+        return True
+    else:
+        return False
 
     
